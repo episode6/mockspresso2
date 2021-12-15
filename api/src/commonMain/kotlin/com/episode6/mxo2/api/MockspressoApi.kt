e9d932bc983d0cb7fceca7d655ddf434c2bd6da6
Geoff Hackett <ghackett@gmail.com>
2021-12-15 12:18:12 -0500

    Define mockspresso2 api (#4)

---
+++ api/src/commonMain/kotlin/com/episode6/mxo2/api/MockspressoApi.kt
@@ -0,0 +1,24 @@
+package com.episode6.mxo2.api
+
+import com.episode6.mxo2.reflect.DependencyKey
+
+interface Dependencies {
+  fun <T : Any?> get(key: DependencyKey<T>): T
+}
+
+sealed class ObjectAnswer {
+  data class Yes(val value: Any?) : ObjectAnswer()
+  object No : ObjectAnswer()
+}
+
+fun interface ObjectMaker {
+  fun makeObject(key: DependencyKey<*>, dependencies: Dependencies): Any?
+}
+
+fun interface OptionalObjectMaker {
+  fun canMakeObject(key: DependencyKey<*>, dependencies: Dependencies): ObjectAnswer
+}
+
+interface FallbackObjectMaker {
+  fun <T : Any?> makeObject(key: DependencyKey<T>): T
+}
