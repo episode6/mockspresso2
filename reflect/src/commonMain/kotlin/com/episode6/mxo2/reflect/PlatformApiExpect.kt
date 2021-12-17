0e5a248d124b23b3a624280d6a0309d97623b984
Geoff Hackett <ghackett@gmail.com>
2021-12-16 20:00:24 -0500

    add comments and split platform files

---
+++ reflect/src/commonMain/kotlin/com/episode6/mxo2/reflect/PlatformApiExpect.kt
@@ -0,0 +1,13 @@
+package com.episode6.mxo2.reflect
+
+import kotlin.reflect.KFunction
+
+/**
+ * Returns true if the receiver is a qualifier annotation
+ */
+expect fun Annotation.isQualifier(): Boolean
+
+/**
+ * Returns a list of [DependencyKey]s that represent the parameters of the receiver [KFunction]
+ */
+expect fun KFunction<*>.parameterKeys(context: TypeToken<*>): List<DependencyKey<*>>
