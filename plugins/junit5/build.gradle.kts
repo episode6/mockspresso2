5dd8c17135c0c5a23ae2ee6f49e8fde98a4d60b2
Geoff Hackett <ghackett@gmail.com>
2022-01-15 13:39:59 -0500

    Add JUnit5 extension

---
+++ plugins/junit5/build.gradle.kts
@@ -0,0 +1,19 @@
+description = "Mockspresso2 plugins for junit5."
+
+plugins {
+  id("config-jvm-deploy")
+}
+
+dependencies {
+  api(project(":api"))
+
+  implementation(libs.junit5.core)
+
+  testImplementation(libs.junit5.core)
+  testImplementation(libs.mockk.core)
+  testImplementation(project(":core"))
+}
+tasks.test {
+  // Enable JUnit 5 (Gradle 4.6+).
+  useJUnitPlatform()
+}
