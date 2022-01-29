d7f04c545099e8d58ee5e6aaeca2621b67bdd46c
Geoff Hackett <ghackett@gmail.com>
2022-01-29 15:42:02 -0500

    make all dependencies potentially dynamic

---
+++ core/src/commonMain/kotlin/com/episode6/mxo2/internal/DependencyCacheBuilder.kt
@@ -0,0 +1,28 @@
+package com.episode6.mxo2.internal
+
+import com.episode6.mxo2.api.Dependencies
+import com.episode6.mxo2.reflect.DependencyKey
+
+private typealias Maker<T> = Dependencies.() -> T
+
+internal class DependencyCacheBuilder {
+  private val map: MutableMap<DependencyKey<*>, Maker<*>> = mutableMapOf()
+
+  fun containsKey(key: DependencyKey<*>): Boolean = map.containsKey(key)
+
+  fun <T : Any?> put(key: DependencyKey<T>, maker: Dependencies.() -> T) {
+    map[key] = maker
+  }
+
+  @Suppress("UNCHECKED_CAST")
+  fun build(makeDependencies: (DependencyValidator)->Dependencies): DependencyCache {
+    val cache = DependencyCache()
+    map.entries.forEach { (key, maker) ->
+      key as DependencyKey<Any?>
+      val validator = DependencyValidator(key)
+      val dependencies = { makeDependencies(validator) }
+      cache.put(key, validator) { dependencies().maker() }
+    }
+    return cache
+  }
+}
