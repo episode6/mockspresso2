4c3937c4be866a8ebea0b579fb8669bf2140aa5c
Geoff Hackett <ghackett@gmail.com>
2022-01-30 15:10:12 -0500

    comments

--- plugins/javax-inject/src/main/kotlin/com/episode6/mxo2/plugins/javax/inject/JavaxInjectPlugins.kt
+++ plugins/javax-inject/src/main/kotlin/com/episode6/mxo2/plugins/javax/inject/JavaxInjectPlugins.kt
@@ -2,6 +2,10 @@ package com.episode6.mxo2.plugins.javax.inject
 
 import com.episode6.mxo2.MockspressoBuilder
 
+/**
+ * Make real objects using reflection according to java.inject rules. Supports constructor, property, field and
+ * method injection.
+ */
 fun MockspressoBuilder.makeRealObjectsUsingJavaxInjectRules(): MockspressoBuilder =
   makeRealObjectsWith(javaxRealObjectMaker())
 
