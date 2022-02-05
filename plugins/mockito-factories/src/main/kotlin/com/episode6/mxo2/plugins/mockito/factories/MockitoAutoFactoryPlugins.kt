3f30aaf9d785f4dbff56746c713f6ef91312e4da
Geoff Hackett <ghackett@gmail.com>
2022-02-05 11:25:35 -0500

    method order

--- plugins/mockito-factories/src/main/kotlin/com/episode6/mxo2/plugins/mockito/factories/MockitoAutoFactoryPlugins.kt
+++ plugins/mockito-factories/src/main/kotlin/com/episode6/mxo2/plugins/mockito/factories/MockitoAutoFactoryPlugins.kt
@@ -7,15 +7,6 @@ import com.episode6.mxo2.reflect.asKClass
 import com.episode6.mxo2.reflect.dependencyKey
 import kotlin.reflect.full.hasAnnotation
 
-/*
- * Mark type [T] (with optional [qualifier]) as a Factory object. The object will be mocked and each method will return
- * a dependency from the underlying Mockspresso instance.
- */
-inline fun <reified T : Any?> MockspressoBuilder.autoFactory(qualifier: Annotation? = null): MockspressoBuilder {
-  val key = dependencyKey<T>(qualifier)
-  return addDependencyOf(key) { autoFactoryMock(key) }
-}
-
 /**
  * Marks any class encountered with the given [A] annotation as a Factory object. The object will be mocked and each
  * method will return a dependency from the underlying Mockspresso instance.
@@ -27,6 +18,15 @@ inline fun <reified A : Annotation> MockspressoBuilder.autoFactoriesByAnnotation
       else                                    -> DynamicObjectMaker.Answer.No
     }
   }
+
+/*
+ * Mark type [T] (with optional [qualifier]) as a Factory object. The object will be mocked and each method will return
+ * a dependency from the underlying Mockspresso instance.
+ */
+inline fun <reified T : Any?> MockspressoBuilder.autoFactory(qualifier: Annotation? = null): MockspressoBuilder {
+  val key = dependencyKey<T>(qualifier)
+  return addDependencyOf(key) { autoFactoryMock(key) }
+}
 
 /**
  * Mark type [T] (with optional [qualifier]) as a Factory object which is also accessible via the returned lazy.
