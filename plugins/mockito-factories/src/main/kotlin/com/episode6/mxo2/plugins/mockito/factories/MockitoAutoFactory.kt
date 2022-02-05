05b6c9e8c46eb6dcf1bd903e60c5550cb8841946
Geoff Hackett <ghackett@gmail.com>
2022-02-05 11:59:50 -0500

    Add mockito factories plugins (#17)
    Adds a new plugin module `plugins-mockito-factories` to supply the functionality from the old `autoMapFactories` plugin in mockspresso1.
    
    Adds extension
     - `MockspressoBuilder.autoFactoriesByAnnotation`
     - `MockspressoBuilder.autoFactory`
     - `MockspressoProperties.autoFactory`
    
    Also includes reflect utils to resolve fully formed TypeTokens from jvm types

---
+++ plugins/mockito-factories/src/main/kotlin/com/episode6/mxo2/plugins/mockito/factories/MockitoAutoFactory.kt
@@ -0,0 +1,33 @@
+package com.episode6.mxo2.plugins.mockito.factories
+
+import com.episode6.mxo2.MockspressoBuilder
+import com.episode6.mxo2.MockspressoProperties
+import com.episode6.mxo2.api.Dependencies
+import com.episode6.mxo2.reflect.DependencyKey
+import com.episode6.mxo2.reflect.asKClass
+import com.episode6.mxo2.reflect.resolveJvmType
+import org.mockito.Mockito
+import org.mockito.stubbing.Answer
+
+/**
+ * Returns a Factory object for the given [factoryKey]. The object will be mocked
+ * and each method will return a dependency from the underlying Mockspresso instance.
+ *
+ * Generally you shouldn't need to access this method directly, prefer applying with [MockspressoBuilder.autoFactory]
+ * or [MockspressoProperties.autoFactory]
+ */
+@Suppress("UNCHECKED_CAST") fun <T : Any?> Dependencies.autoFactoryMock(factoryKey: DependencyKey<T>): T =
+  Mockito.mock(factoryKey.token.asKClass().java as Class<T>, mockitoAutoFactoryAnswer(factoryKey))
+
+/**
+ * Returns a mockito default [Answer] for use in a mock of the given [factoryKey]. The answer will resolve the return
+ * type of the called method at runtime and return a dependency from the mockspresso graph.
+ */
+fun Dependencies.mockitoAutoFactoryAnswer(factoryKey: DependencyKey<*>): Answer<Any> = Answer<Any> { invoc ->
+  when (invoc.method.returnType) {
+    Void.TYPE -> null
+    else      -> factoryKey.token
+      .resolveJvmType(invoc.method.genericReturnType, invoc.method.declaringClass)
+      .let { get(DependencyKey(it, factoryKey.qualifier)) }
+  }
+}
