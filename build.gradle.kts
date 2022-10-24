4d573a09fd5880aac07da54f59d42dc084523e97
Geoff Hackett <ghackett@gmail.com>
2022-10-24 12:03:45 -0400

    [VERSION] Release v2.0.0-beta01 (#83)

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 
 allprojects {
   group = "com.episode6.mockspresso2"
-  version = "2.0.0-SNAPSHOT"
+  version = "2.0.0-beta01"
 }
 description =
   "A testing tool designed to reduce friction, boiler-plate and brittleness in unit tests. It's like dependency injection for your tests!"
