759966c2c0ec3e121dc755cdef02fc349184182a
Geoff Hackett <ghackett@gmail.com>
2023-03-09 19:24:42 -0500

    test

---
+++ core/src/commonTest/kotlin/com/episode6/mockspresso2/EnsureInitWithNoInstanceTest.kt
@@ -0,0 +1,32 @@
+package com.episode6.mockspresso2
+
+import assertk.assertThat
+import assertk.assertions.isFalse
+import assertk.assertions.isTrue
+import kotlin.test.Test
+
+class EnsureInitWithNoInstanceTest {
+  private val mxo = MockspressoBuilder().build()
+
+  private var depCreated = false
+  private val dep by mxo.dependency {
+    depCreated = true
+    Unit
+  }
+
+  @Test fun testNoInitNoTouch() {
+    assertThat(depCreated).isFalse()
+  }
+
+  @Test fun testEnsureInit() {
+   mxo.ensureInit()
+
+   assertThat(depCreated).isTrue()
+  }
+
+  @Test fun testTouch() {
+    dep
+
+    assertThat(depCreated).isTrue()
+  }
+}
