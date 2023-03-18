53fd790d1c2b8a9aa0567f8bc0f8c7b964bbccca
Geoff Hackett <ghackett@gmail.com>
2023-03-18 14:54:13 -0400

    prototyping v3, kill the builder

--- core/src/commonMain/kotlin/com/episode6/mockspresso2/Builder.kt
+++ core/src/commonMain/kotlin/com/episode6/mockspresso2/Builder.kt
@@ -1,8 +1,15 @@
 package com.episode6.mockspresso2
 
-import com.episode6.mockspresso2.internal.MockspressoBuilderContainer
+import com.episode6.mockspresso2.internal.MockspressoContainer
+import com.episode6.mockspresso2.internal.MockspressoPropertiesContainer
+
 
 /**
- * Root entry point to create a new [MockspressoBuilder]
+ * Root entry point to create a new [Mockspresso] instance
  */
-fun MockspressoBuilder(): MockspressoBuilder = MockspressoBuilderContainer()
+fun Mockspresso(): Mockspresso = MockspressoContainer(MockspressoPropertiesContainer())
+
+/**
+ * Root entry point to create a new [Mockspresso] instance with a convenience apply block
+ */
+fun Mockspresso(initBlock: MockspressoProperties.() -> Unit): Mockspresso = Mockspresso().apply(initBlock)
