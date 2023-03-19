d15de7635742b64e1cbdb47f51f1ec522a8efbab
Geoff Hackett <ghackett@gmail.com>
2023-03-19 16:06:04 -0400

    fix mockito-factories plugin

--- plugins/mockito-factories/src/main/kotlin/com/episode6/mockspresso2/plugins/mockito/factories/MockitoAutoFactoryPlugins.kt
+++ plugins/mockito-factories/src/main/kotlin/com/episode6/mockspresso2/plugins/mockito/factories/MockitoAutoFactoryPlugins.kt
@@ -1,6 +1,5 @@
 package com.episode6.mockspresso2.plugins.mockito.factories
 
-import com.episode6.mockspresso2.MockspressoBuilder
 import com.episode6.mockspresso2.MockspressoProperties
 import com.episode6.mockspresso2.api.DynamicObjectMaker
 import com.episode6.mockspresso2.plugins.mockito.factories.reflect.autoFactoryMock
@@ -12,21 +11,13 @@ import kotlin.reflect.full.hasAnnotation
  * Marks any class encountered with the given [A] annotation as a Factory object. The object will be mocked and each
  * method will return a dependency from the underlying Mockspresso instance.
  */
-inline fun <reified A : Annotation> MockspressoBuilder.autoFactoriesByAnnotation(): MockspressoBuilder =
+inline fun <reified A : Annotation> MockspressoProperties.autoFactoriesByAnnotation() {
   addDynamicObjectMaker { key, deps ->
     when {
       key.token.asKClass().hasAnnotation<A>() -> DynamicObjectMaker.Answer.Yes(deps.autoFactoryMock(key))
       else                                    -> DynamicObjectMaker.Answer.No
     }
   }
-
-/*
- * Mark type [T] (with optional [qualifier]) as a Factory object. The object will be mocked and each method will return
- * a dependency from the underlying Mockspresso instance.
- */
-inline fun <reified T : Any?> MockspressoBuilder.autoFactory(qualifier: Annotation? = null): MockspressoBuilder {
-  val key = dependencyKey<T>(qualifier)
-  return dependency(key) { autoFactoryMock(key) }
 }
 
 /**
